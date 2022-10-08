af8a41dd89f9d34e383f52a2e13439aa342a1b9c
Ali Jafari <jafariali694@gmail.com>
2022-10-08 21:50:32 +0330

    Update data.py

--- data.py
+++ data.py
@@ -7,7 +7,34 @@ import cv2
 from tqdm import tqdm
 from glob import glob
 from sklearn.model_selection import train_test_split
-from albumentations import CenterCrop, RandomRotate90, GridDistortion, HorizontalFlip, VerticalFlip
+from albumentations import (
+    PadIfNeeded,
+    HorizontalFlip,
+    VerticalFlip,
+    CenterCrop,
+    Crop,
+    Compose,
+    Transpose,
+    RandomRotate90,
+    ElasticTransform,
+    GridDistortion,
+    OpticalDistortion,
+    RandomSizedCrop,
+    OneOf,
+    CLAHE,
+    RandomBrightnessContrast,
+    RandomGamma,
+    HueSaturationValue,
+    RGBShift,
+    RandomBrightness,
+    RandomContrast,
+    MotionBlur,
+    MedianBlur,
+    GaussianBlur,
+    GaussNoise,
+    ChannelShuffle,
+    CoarseDropout
+)
 
 def create_dir(path):
     """ Create a directory. """
@@ -21,6 +48,8 @@ def create_dir(path):
 def augment_data(images, masks, save_path, augment=True):
     H = 256
     W = 256
+    size = (H, W)
+    crop_size = (H-32, W-32)
 
     for x, y in tqdm(zip(images, masks), total=len(images)):
         name = x.split("/")[-1].split(".")
@@ -32,39 +61,166 @@ def augment_data(images, masks, save_path, augment=True):
         mask_name = name[0]
         mask_extn = name[1]
 
-        """ Reading image and mask. """
+        """ Reading image and mask, and resizing. """
         x = cv2.imread(x, cv2.IMREAD_COLOR)
         y = cv2.imread(y, cv2.IMREAD_COLOR)
 
+        x = cv2.resize(x, (H, W))
+        y = cv2.resize(y, (H, W))
         """ Augmentation """
         if augment == True:
-            aug = CenterCrop(H, W, p=1.0)
+            aug = CenterCrop(p=1, height=crop_size[0], width=crop_size[1])
             augmented = aug(image=x, mask=y)
             x1 = augmented["image"]
             y1 = augmented["mask"]
 
-            aug = RandomRotate90(p=1.0)
+            ## Crop
+            x_min = 0
+            y_min = 0
+            x_max = x_min + crop_size[0]
+            y_max = y_min + crop_size[1]
+
+            aug = Crop(p=1, x_min=x_min, x_max=x_max, y_min=y_min, y_max=y_max)
             augmented = aug(image=x, mask=y)
             x2 = augmented['image']
             y2 = augmented['mask']
 
-            aug = GridDistortion(p=1.0)
+            ## Random Rotate 90 degree
+            aug = RandomRotate90(p=1)
             augmented = aug(image=x, mask=y)
             x3 = augmented['image']
             y3 = augmented['mask']
 
-            aug = HorizontalFlip(p=1.0)
+            ## Transpose
+            aug = Transpose(p=1)
             augmented = aug(image=x, mask=y)
             x4 = augmented['image']
             y4 = augmented['mask']
 
-            aug = VerticalFlip(p=1.0)
+            ## ElasticTransform
+            aug = ElasticTransform(p=1, alpha=120, sigma=120 * 0.05, alpha_affine=120 * 0.03)
             augmented = aug(image=x, mask=y)
             x5 = augmented['image']
             y5 = augmented['mask']
 
-            save_images = [x, x1, x2, x3, x4, x5]
-            save_masks =  [y, y1, y2, y3, y4, y5]
+            ## Grid Distortion
+            aug = GridDistortion(p=1)
+            augmented = aug(image=x, mask=y)
+            x6 = augmented['image']
+            y6 = augmented['mask']
+
+            ## Optical Distortion
+            aug = OpticalDistortion(p=1, distort_limit=2, shift_limit=0.5)
+            augmented = aug(image=x, mask=y)
+            x7 = augmented['image']
+            y7 = augmented['mask']
+
+            ## Vertical Flip
+            aug = VerticalFlip(p=1)
+            augmented = aug(image=x, mask=y)
+            x8 = augmented['image']
+            y8 = augmented['mask']
+
+            ## Horizontal Flip
+            aug = HorizontalFlip(p=1)
+            augmented = aug(image=x, mask=y)
+            x9 = augmented['image']
+            y9 = augmented['mask']
+
+            ## Grayscale
+            x10 = cv2.cvtColor(x, cv2.COLOR_RGB2GRAY)
+            y10 = y
+
+            ## Grayscale Vertical Flip
+            aug = VerticalFlip(p=1)
+            augmented = aug(image=x10, mask=y10)
+            x11 = augmented['image']
+            y11 = augmented['mask']
+
+            ## Grayscale Horizontal Flip
+            aug = HorizontalFlip(p=1)
+            augmented = aug(image=x10, mask=y10)
+            x12 = augmented['image']
+            y12 = augmented['mask']
+
+            ## Grayscale Center Crop
+            aug = CenterCrop(p=1, height=crop_size[0], width=crop_size[1])
+            augmented = aug(image=x10, mask=y10)
+            x13 = augmented['image']
+            y13 = augmented['mask']
+
+            ##
+            aug = RandomBrightnessContrast(p=1)
+            augmented = aug(image=x, mask=y)
+            x14 = augmented['image']
+            y14 = augmented['mask']
+
+            aug = RandomGamma(p=1)
+            augmented = aug(image=x, mask=y)
+            x15 = augmented['image']
+            y15 = augmented['mask']
+
+            aug = HueSaturationValue(p=1)
+            augmented = aug(image=x, mask=y)
+            x16 = augmented['image']
+            y16 = augmented['mask']
+
+            aug = RGBShift(p=1)
+            augmented = aug(image=x, mask=y)
+            x17 = augmented['image']
+            y17 = augmented['mask']
+
+            aug = RandomBrightness(p=1)
+            augmented = aug(image=x, mask=y)
+            x18 = augmented['image']
+            y18 = augmented['mask']
+
+            aug = RandomContrast(p=1)
+            augmented = aug(image=x, mask=y)
+            x19 = augmented['image']
+            y19 = augmented['mask']
+
+            aug = MotionBlur(p=1, blur_limit=7)
+            augmented = aug(image=x, mask=y)
+            x20 = augmented['image']
+            y20 = augmented['mask']
+
+            aug = MedianBlur(p=1, blur_limit=11)
+            augmented = aug(image=x, mask=y)
+            x21 = augmented['image']
+            y21 = augmented['mask']
+
+            aug = GaussianBlur(p=1, blur_limit=(3, 11))
+            augmented = aug(image=x, mask=y)
+            x22 = augmented['image']
+            y22 = augmented['mask']
+
+            aug = GaussNoise(p=1)
+            augmented = aug(image=x, mask=y)
+            x23 = augmented['image']
+            y23 = augmented['mask']
+
+            aug = ChannelShuffle(p=1)
+            augmented = aug(image=x, mask=y)
+            x24 = augmented['image']
+            y24 = augmented['mask']
+
+            aug = CoarseDropout(p=1, max_holes=8, max_height=32, max_width=32)
+            augmented = aug(image=x, mask=y)
+            x25 = augmented['image']
+            y25 = augmented['mask']
+
+            save_images = [
+            x, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10,
+            x11, x12, x13, x14, x15, x16, x17, x18, x19, x20,
+            x21, x22, x23, x24, x25
+            ]
+            save_masks  = [
+            y, y1, y2, y3, y4, y5, y6, y7, y8, y9, y10,
+            y11, y12, y13, y14, y15, y16, y17, y18, y19, y20,
+            y21, y22, y23, y24, y25
+            ]
+
 
         else:
             save_images = [x]
@@ -116,18 +272,16 @@ def load_data(path,split=0.1):
 if __name__ == "__main__":
     """ Loading original images and masks. """
     np.random.seed(42)
-    path = "/content/drive/MyDrive/CVC-ClinicDB/"
+    path = "/content/clinic/"
     (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(path, split=0.1)
 
-    create_dir("/content/drive/MyDrive/new_data/train/images/")
-    create_dir("/content/drive/MyDrive/new_data/train/masks/")
-    create_dir("/content/drive/MyDrive/new_data/valid/images/")
-    create_dir("/content/drive/MyDrive/new_data/valid/masks/")
-    create_dir("/content/drive/MyDrive/new_data/test/images/")
-    create_dir("/content/drive/MyDrive/new_data/test/masks/")
+    create_dir("/content/clinic_augmented/train/images/")
+    create_dir("/content/clinic_augmented/train/masks/")
+    create_dir("/content/clinic_augmented/valid/images/")
+    create_dir("/content/clinic_augmented/valid/masks/")
+    create_dir("/content/clinic_augmented/test/images/")
+    create_dir("/content/clinic_augmented/test/masks/")
 
-    augment_data(train_x, train_y, "/content/drive/MyDrive/new_data/train/", augment=True)
-    augment_data(valid_x, valid_y, "/content/drive/MyDrive/new_data/valid/", augment=False)
-    augment_data(test_x, test_y, "/content/drive/MyDrive/new_data/test/", augment=False)
-
-
+    augment_data(train_x, train_y, "/content/clinic_augmented/train/", augment=True)
+    augment_data(valid_x, valid_y, "/content/clinic_augmented/valid/", augment=False)
+    augment_data(test_x, test_y, "/content/clinic_augmented/test/", augment=False)
